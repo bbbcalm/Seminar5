2870d07f39e7f7ad5f7b2b01e97be9d7ae831996
Miraziz <wiut00016603@gmail.com>
2023-09-25 12:52:21 +0500

    binarys proj

---
+++ binarys.py
@@ -0,0 +1,15 @@
+# Calculate Mark function
+# def CalculateMark(weight, mark):
+#     return weight * mark
+#
+#
+# cwWeight = 0.4
+# cwMark = 65
+# CalculateMark(cwWeight, cwMark)
+# CalculateMark(0.6, 55)
+
+
+
+decimal_num = input("Put your decimal number: ")
+decimal_to_binary = bin(int(decimal_num))
+print( f"Result: {decimal_to_binary}" )
